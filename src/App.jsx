e31581f5b25eb68578f9ad215c6859eaecf98efb
vijay hitnalli <vijayhitnalli35@gmail.com>
2024-02-13 16:57:05 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   
     <div>
     <Header/>
+    
     </div>
      
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Component/Util/Login.jsx'
+import Seller from './Component/Util/Seller.jsx'
+import Cart from './Component/Util/Cart.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/seller' element={<Seller/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+
+      
+    </Routes>
     </BrowserRouter>
     
   </React.StrictMode>,
