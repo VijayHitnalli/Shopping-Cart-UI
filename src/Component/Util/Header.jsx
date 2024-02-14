f45807ecaf12da2730e32c5235736433c65b3181
vijay hitnalli <vijayhitnalli35@gmail.com>
2024-02-14 23:00:00 +0530

    routes prtotype

--- src/Component/Util/Header.jsx
+++ src/Component/Util/Header.jsx
@@ -3,31 +3,37 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='p-4 bg-orange-400 h-20'>
-      <nav className='flex justify-between items-center'>
+    <header className='p-2 bg-white shadow-xl h-20'>
+      <nav className='container mx-auto flex justify-between items-center'>
         {/* Logo */}
         <Link to="/">
-          <img src="https://img.freepik.com/premium-vector/online-shop-logo-design-template-perfect-ecommerce-sale-store-shop-discount-web_695238-64.jpg" className='h-10 px-7' alt="logo" />
+          <img src="https://i.pinimg.com/736x/61/77/cc/6177cc502e93e1e3226519e43ff332d5.jpg" className='px-20 h-16 w-15' alt="logo" />
         </Link>
         
         {/* Search bar */}
         <div>
-          <input type="text" placeholder="Search for products, categories, etc..." className='h-8 w-96 border rounded' />
+        <i className="fa-solid fa-magnifying-glass float-left position absolute pl-3 py-3.5 text-gray-500" ></i><input type="text" placeholder="Search for products, categories, etc..." className='h-10 w-96 rounded-tl-md rounded-br-lg px-9 bg-gray-200 ' />
         </div>
 
         {/* Link block */}
-        <div className='space-x-12'>
+        <div className='flex mr-20 space-x-11'>
           {/* Login */}
-          <Link to="/login" className='text-blac'>Log in</Link>
+          <i className="fa-regular fa-user ml-6 my-1.5 float-left position absolute"></i> <Link to="/login" className='text-black'>Log in</Link>
           
           {/* Become a seller */}
-          <Link to="/seller" className='text-black text-heigh'>Become a seller</Link>
+          <Link to="/seller/register" className='text-black'>Become a seller</Link>
           
           {/* Cart */}
-          <Link to="/cart" className='text-black'><i className="fa-solid fa-cart-shopping"></i> Cart</Link>
+          <Link to="/cart" className='text-black flex items-center'>
+            <i className="fas fa-cart-shopping mr-1"></i> Cart
+          </Link>
+
+          {/* <Link to="/wishlist" className='text-black flex items-center'>
+            <i className="fas fa-cart-shopping mr-1"></i> Wishlist
+          </Link> */}
           
           {/* Some additional icon or link */}
-          <Link to="/" className='text-black '>
+          <Link to="/" className='text-black'>
             <i className="fas fa-grip-lines text-lg"></i>
           </Link>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Component/Util/Login.jsx'
-import Seller from './Component/Util/Seller.jsx'
-import Cart from './Component/Util/Cart.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+import navs from './Component/Util/Routes/Navigation.jsx';
+import Home from './Component/Util/Public/Home.jsx';
+
+const user = {
+  username: "",
+  role: "CUSTOMER",
+  isAuthenticated: false
+}
+
+const {role,isAuthenticated}= user;
+
+const allRoutes=()=>{
+    return(
+        <Route path="/" element={<App />}>
+      {navs.map((nav,i)=>{
+        
+        if(isAuthenticated){
+            if(nav.isVisibleAfterAuth){
+                if(nav.role ===role || nav.role==="All"){
+                    console.log(nav)
+                return <Route key={i} path={nav.path} element={nav.element}/>
+                }
+            }
+        }else{
+            if(!nav.requireAuth && nav.role==="ALL"){
+                console.log(nav)
+                return <Route key={i} path={nav.path} element={nav.element}/>
+            }
+        }
+      })}
+        </Route>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
+  <BrowserRouter>
     <Routes>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/seller' element={<Seller/>}></Route>
-      <Route path='/cart' element={<Cart/>}></Route>
-
-      
+    {allRoutes()}
     </Routes>
-    </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
